47e1f460174f6a4ada3ba8d6ff773e13cf4c4e8d
mikeamundsen <mikeamundsen@723031c0-a919-0410-adff-8f8ff343f446>
2008-08-19 08:39:09 +0000

    Messages.cs   * fixed content-type for RSS messages-rss.xsl   * fixed XSL output ot clean up RSS SSDSDeploy   * added new project to deploy binary files to SSDS store

---
+++ trunk/ssds/SSDSDeploy/bin/Debug/upload.cmd
@@ -0,0 +1,7 @@
+@echo off
+rem sample using ssds deploy console app
+
+ssdsdeploy /binaries/test1/img01 "C:\Projects\SSDS\SSDSDeploy\images\water-lilies.jpg"
+ssdsdeploy /binaries/test1/img02 "C:\Projects\SSDS\SSDSDeploy\images\blue-hills.jpg"
+ssdsdeploy /binaries/test1/img03 "C:\Projects\SSDS\SSDSDeploy\images\sunset.jpg"
+ssdsdeploy /binaries/test1/img04 "C:\Projects\SSDS\SSDSDeploy\images\winter.jpg"
