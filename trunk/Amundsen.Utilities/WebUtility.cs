b8c9a1cd0df32354eb3628cb63f1b567337e4b5b
mca.amundsen@gmail.com <mca.amundsen@gmail.com@723031c0-a919-0410-adff-8f8ff343f446>
2010-01-11 22:46:33 +0000

    updated for public use

--- trunk/Amundsen.Utilities/WebUtility.cs
+++ trunk/Amundsen.Utilities/WebUtility.cs
@@ -9,7 +9,7 @@ using System.Globalization;
 namespace Amundsen.Utilities
 {
   /// <summary>
-  /// Public Domain 2008 amundsen.com, inc.
+  /// Public Domain 2008-2010 amundsen.com, inc.
   /// @author mike amundsen (mamund@yahoo.com)
   /// @version 1.0 (2008-07-03)
   /// </summary>
